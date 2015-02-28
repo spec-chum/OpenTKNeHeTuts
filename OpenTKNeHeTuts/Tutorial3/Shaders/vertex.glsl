c68457bca329d8a29d37bdf1c8e52ea277eef818
Spec-Chum <specchumster@gmail.com>
2015-02-28 23:10:27 +0000

    Clean up VS

--- OpenTKNeHeTuts/Tutorial3/Shaders/vertex.glsl
+++ OpenTKNeHeTuts/Tutorial3/Shaders/vertex.glsl
@@ -4,12 +4,11 @@ layout(location = 0) in vec3 vertPos;
 layout(location = 1) in vec3 vertColour;
 
 uniform mat4 MVP;
-uniform vec3 translate;
 
 out vec3 colour;
 
 void main()
 {
 	colour = vertColour;
-	gl_Position = MVP * vec4(vertPos + translate, 1.0);
+	gl_Position = MVP * vec4(vertPos, 1.0);
 }
