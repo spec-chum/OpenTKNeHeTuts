37a4a5f7e6b6e8ea2e52f8219e16028e9db736e7
Spec-Chum <specchumster@gmail.com>
2015-02-28 15:53:43 +0000

    Move camera a little forward to match NeHe

--- OpenTKNeHeTuts/Tutorial2/Game.cs
+++ OpenTKNeHeTuts/Tutorial2/Game.cs
@@ -76,7 +76,7 @@ namespace OpenTKNeHeTut2
             // Initialise MVP Matrix
             float ar = (float)ClientSize.Width / (float)ClientSize.Height;
             projectionMatrix4 = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, ar, 0.1f, 1000.0f);
-            modelMatrix4 = Matrix4.LookAt(0, 0.0f, 1.0f, 0, 0, 0, 0, 1.0f, 0);
+            modelMatrix4 = Matrix4.LookAt(0, 0.0f, 0.1f, 0, 0, 0, 0, 1.0f, 0);
             Matrix4 MVP = modelMatrix4 * projectionMatrix4;
             GL.UniformMatrix4(MVPLocation, false, ref MVP);
             GL.UseProgram(0);
