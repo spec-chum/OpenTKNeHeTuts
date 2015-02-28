77f1ac05b5d94868eb573a9b40d8d9469a42af6b
Spec-Chum <specchumster@gmail.com>
2015-02-28 22:13:27 +0000

    Cosmetics

--- OpenTKNeHeTuts/Tutorial3/Game.cs
+++ OpenTKNeHeTuts/Tutorial3/Game.cs
@@ -17,17 +17,17 @@ namespace OpenTKNeHeTut3
 	{
 		private float[] triangle =
 		{
-			0.0f, 1.0f, 0.0f,  // Top
+			 0.0f, 1.0f, 0.0f,  // Top
 			-1.0f,-1.0f, 0.0f,  // Bottom Left
-			1.0f,-1.0f, 0.0f   // Bottom Right
+			 1.0f,-1.0f, 0.0f   // Bottom Right
 		};
 
 		private float[] square =
 		{
 			-1.0f, -1.0f, 0.0f, // Bottom left
 			-1.0f,  1.0f, 0.0f, // Top left            
-			1.0f, -1.0f, 0.0f, // Bottom right
-			1.0f,  1.0f, 0.0f  // Top right             
+			 1.0f, -1.0f, 0.0f, // Bottom right
+			 1.0f,  1.0f, 0.0f  // Top right             
 		};
 
 		private bool isFullscreen = false;
